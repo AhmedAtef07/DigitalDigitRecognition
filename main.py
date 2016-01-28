c561572fd31b8164791e643d460cbff7e242818a
AhmedAtef07 <ahmedatef07@hotmail.com>
2016-01-28 17:22:03 +0200

    Finishing up for release.

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import math
 import operator
 import sys
 
+# Local import
 import image_processing as imgp
 import pattern_detection as pd
 
@@ -15,15 +16,19 @@ train_raw_set, test_raw_set = [], []
 ################################## TESTING FROM CACHED FILE ##################################
 if sys.argv[1] == "lc":
   pd.load_from_cache(sys.argv[2])
-  test_raw_set = imgp.get_train_set("test")
+  test_raw_set = imgp.get_train_set("test/1")
 
   for test_img in test_raw_set:
     x = pd.detect(test_img["data"])
 
-    for k in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]:
-      print "K = %2d: Expected %d" % (k, pd.nearest_of_k_neighbors(x, k))
+    for k in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 200]:
+      # print pd.nearest_of_k_neighbors(x, k)
+      print "K = %2d: Expected %s" % (k, pd.nearest_of_k_neighbors(x, k))
     imgp.plot_img(test_img["data"])
     print "=" * 20
+
+   
+   
 
 elif sys.argv[1] == "lcsv":
   pd.load_from_cache(sys.argv[2])
@@ -35,21 +40,30 @@ elif sys.argv[1] == "lcsv":
   k_misses = defaultdict(lambda: 0)
 
   count = 0 
-
+  ne = [1, 3, 20, 30, 50, 70, 100, 200, 500, 1000, 1500, 4000, 6000, 7000, 10000]
   for test_img in test_raw_set:
     x = pd.detect(test_img["data"])
     # nearest_neighbours = [i["value"] for i in pd.detect(test_img["data"])[:10]]
     
 
-    for k in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]:
+    for k in ne:
       if test_img["value"] != pd.nearest_of_k_neighbors(x, k):
         k_misses[k] += 1
     # if test_img["value"] != nearest_neighbours[0]:
     #   imgp.plot_img(test_img["data"])
 
     count += 1
-    if count % 100:
-      print "#%d, %d -> %d; Accuracy: %.2f%%, Correct: %d, Wrong: %d" % (count, test_img["value"], x[0]["value"], 100 - k_misses[1] / float(count) * 100, count - k_misses[1], k_misses[1])
+    # print "#%d, %d -> %d; Accuracy: %.2f%%, Correct: %d, Wrong: %d" % (count, test_img["value"], x[0]["value"], 100 - k_misses[1] / float(count) * 100, count - k_misses[1], k_misses[1])
+    for k in ne:
+      print "#%d, %d -> %d; K = %4d, Correct: %d, Wrong: %d, Accuracy: %.2f%%" % (
+        count, 
+        test_img["value"],
+        pd.nearest_of_k_neighbors(x, k), 
+        k,
+        count - k_misses[k], 
+        k_misses[k],
+        100 - k_misses[k] / float(count) * 100)
+    print "-" * 17 
     
   print "=" * 20
 
@@ -76,7 +90,8 @@ else:
   if len(sys.argv) > 3: cache_file_name = sys.argv[3]
 
   t = time.time()
-  raw_train_set = imgp.get_csv_train_set("../mnist_train.csv", lines_count)
+  # raw_train_set = imgp.get_csv_train_set("../mnist_train.csv", lines_count)
+  raw_train_set = imgp.get_train_set("../data_digital_digits", lines_count)
   print len(raw_train_set), "samples are in the raw_train_set."
   print "Parsing images completed in %d seconds." % (time.time() - t)
   # 18 sec
